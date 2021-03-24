74b384e3db87495a8f8becba801c286aa53ce6ab
baxodir <boxa.developer@gmail.com>
2021-03-24 16:29:54 +0500

    Hash Route

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import base64
 import io
 from PIL import Image
 from io import BytesIO
+import os
 
 app = Flask(__name__)
 
@@ -21,20 +22,20 @@ def index():
     return 'All OK, 200'
 
 
-@app.before_request
-def middleware():
-    if request.path.split('/')[1] == 'kvlite':
-        drive = request.full_path.split('/')[2].split(':')[0]
-        file = request.full_path.split(':')[-1][:-1]
-        return redirect(f"/process/{drive}/{file.replace('/', '_')}")
+# @app.before_request
+# def middleware():
+#     if request.path.split('/')[1] == 'kvlite':
+#         drive = request.full_path.split('/')[2].split(':')[0]
+#         file = request.full_path.split(':')[-1][:-1]
+#         return redirect(f"/process/{drive}/{file.replace('/', '_')}")
 
 
-@app.route('/process/<drive>/<text>', methods=['GET'])
-def get_image(drive, text):
-    text = text.replace('_', '/')
-    # return text.encode()
-    buffer = byteio_to_image(text)
-    return Response(buffer.getvalue(), mimetype='image/jpeg')
+@app.route('/img/<hash>', methods=['GET'])
+def get_image(hash):
+    base_path = '/home/fs_files/'
+    drive, filename = hash.split(':')
+    return Response(f'Drive: {drive} File: {filename} File Path: {os.path.join(base_path, drive, filename)}')
+    # return Response(, mimetype='image/jpeg')
 
 
 if __name__ == '__main__':
