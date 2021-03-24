25ffb12acf5a87a21aa19d1c611067169a4b7ff4
baxodir <boxa.developer@gmail.com>
2021-03-24 17:05:04 +0500

    Open Image

--- app.py
+++ app.py
@@ -10,36 +10,20 @@ import os
 app = Flask(__name__)
 
 
-def byte_to_buffer(image):
-    # img = Image.open(io.BytesIO(image))
-    buffer = io.BytesIO(image)
-    # img.save(buffer, format="JPEG")
-    return buffer
-
-
 @app.route('/')
 def index():
     return 'All OK, 200'
 
 
-# @app.before_request
-# def middleware():
-#     if request.path.split('/')[1] == 'kvlite':
-#         drive = request.full_path.split('/')[2].split(':')[0]
-#         file = request.full_path.split(':')[-1][:-1]
-#         return redirect(f"/process/{drive}/{file.replace('/', '_')}")
-
-
-@app.route('/img/<hash>', methods=['GET'])
-def get_image(hash):
+@app.route('/img/<hash_url>', methods=['GET'])
+def get_image(hash_url):
     base_path = '/home/fs_files/'
-    drive, filename = hash.split(':')
-    file_path = os.path.join(base_path, drive, hash)
+    drive, filename = hash_url.split(':')
+    file_path = os.path.join(base_path, drive, hash_url)
     with open(file_path, 'rb') as image_file:
-        img = image_file
-    # return Response(f'Drive: {drive} File: {filename} File Path: {os.path.join(base_path, drive, hash)}')
-    return send_file(io.BytesIO(img.read()),
-                     attachment_filename=hash+'.jpg',
+        img = io.BytesIO(image_file.read())
+    return send_file(img,
+                     attachment_filename=hash + '.jpg',
                      mimetype='image/jpeg')
 
 
