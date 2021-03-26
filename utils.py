467797fc611a81845e518e67c5525f24db25766c
baxodir <boxa.developer@gmail.com>
2021-03-26 17:26:02 +0500

    Scale

--- utils.py
+++ utils.py
@@ -55,7 +55,7 @@ def resize_scale(*arguments):
     width, height = img.size
     print(img.size)
     img.resize((int(width*scale), int(height*scale)), Image.ANTIALIAS)
-    print(img.size)
+    print(img.size, scale)
     return img
 
 
